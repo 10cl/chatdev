c0fbd0ab8c12f454b86e42fcc68997d76588df5b
wong2 <wonderfuly@gmail.com>
2023-07-31 16:35:03 +0800

    Remove console logs for production build
    (cherry picked from commit ca9e0278b8ad95a33af8176e007968cc5a7a8558)

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,25 @@ import manifest from './manifest.config'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
       tsconfigPaths(),
       react(),
-      crx({ manifest }),
+      crx({manifest}),
       viteStaticCopy({
-          targets: [
-              { src: 'src/assets/ex_assets/*', dest: './assets/'}
-          ],
+        targets: [
+          {src: 'src/assets/ex_assets/*', dest: './assets/'}
+        ],
       }),
-  ],
-  build: {
-    rollupOptions: {
-      input: ['app.html'],
+    ],
+    build: {
+      rollupOptions: {
+        input: ['app.html'],
+      },
     },
-  },
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : [],
+    },
+  }
 })
