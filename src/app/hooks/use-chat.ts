c51844372c23431f327c51b019d350ca91be2214
10cl <notice@toscl.com>
2023-10-06 15:57:52 +0800

    feat(input): message update

--- src/app/hooks/use-chat.ts
+++ src/app/hooks/use-chat.ts
@@ -5,10 +5,9 @@ import { chatFamily } from '~app/state'
 import { setConversationMessages } from '~services/chat-history'
 import { ChatMessageModel } from '~types'
 import { uuid } from '~utils'
+import { ChatError } from '~utils/errors'
 import { BotId } from '../bots'
-import store from "store2";
-import {requestHostPermission} from "~app/utils/permissions";
-import {ChatError, ErrorCode} from "~utils/errors";
+import store from "store2"
 
 export function useChat(botId: BotId) {
   const chatAtom = useMemo(() => chatFamily({ botId, page: 'singleton' }), [botId])
@@ -38,39 +37,45 @@ export function useChat(botId: BotId) {
         draft.generatingMessageId = botMessageId
         draft.abortController = abortController
       })
-      await chatState.bot.sendMessage({
-        prompt: input,
-        signal: abortController.signal,
-        onEvent(event) {
-          if (event.type === 'UPDATE_ANSWER') {
-            store.set("input_text_message", "")
-            updateMessage(botMessageId, (message) => {
-              message.text = event.data.text
-            })
-            store.set("response_update_text", event.data.text)
-          } else if (event.type === 'ERROR') {
-            console.error('sendMessage error', event.error.code, event.error)
-            updateMessage(botMessageId, (message) => {
-              message.error = event.error
-            })
-            setChatState((draft) => {
-              draft.abortController = undefined
-              draft.generatingMessageId = ''
-            })
-          } else if (event.type === 'DONE') {
-            store.set("input_text_message", "")
-            setChatState((draft) => {
-              draft.abortController = undefined
-              draft.generatingMessageId = ''
-            })
-          }
-        },
+
+      const resp = await chatState.bot.sendMessage({
+          prompt: input,
+          signal: abortController.signal,
       })
+
+        try {
+            store.set("input_text_message", "")
+            for await (const text of resp) {
+                updateMessage(botMessageId, (message) => {
+                    message.text = text
+                    store.set("response_update_text", text)
+                })
+            }
+            store.set("input_text_message", "")
+        } catch (err: unknown) {
+            if (!abortController.signal.aborted) {
+                abortController.abort()
+            }
+            const error = err as ChatError
+            console.error('sendMessage error', error.code, error)
+            updateMessage(botMessageId, (message) => {
+                message.error = error
+            })
+            setChatState((draft) => {
+                draft.abortController = undefined
+                draft.generatingMessageId = ''
+            })
+        }
+
+        setChatState((draft) => {
+            draft.abortController = undefined
+            draft.generatingMessageId = ''
+        })
     },
-    [botId, chatState.bot, setChatState, updateMessage],
+      [botId, chatState.bot, setChatState, updateMessage],
   )
 
-  const resetConversation = useCallback(() => {
+    const resetConversation = useCallback(() => {
     chatState.bot.resetConversation()
     setChatState((draft) => {
       draft.abortController = undefined
