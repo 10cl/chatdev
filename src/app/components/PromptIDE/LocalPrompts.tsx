4fa3be0ec33be2f0c196d49d512e9f35a8b97959
10cl <notice@toscl.com>
2024-01-04 20:47:59 +0800

    feat(editor): #23  Prompt IDE can split in horizontal/vertical and support resize.

--- src/app/components/PromptIDE/LocalPrompts.tsx
+++ src/app/components/PromptIDE/LocalPrompts.tsx
@@ -1,6 +1,6 @@
-import {FC, useCallback, useState} from 'react'
+import {FC, LegacyRef, useCallback, useRef, useState} from 'react'
 import cx from 'classnames'
-import './main.css'
+import '../../main.css'
 import React from "react";
 import {useTranslation} from "react-i18next";
 import useSWR from "swr";
@@ -17,9 +17,9 @@ import AceEditor from "react-ace";
 import Button from "~app/components/Button";
 import {useAtom} from "jotai/index";
 import {
-    editorFocusAtom,
+    editorFocusAtom, editorHeightAtom,
     editorPromptAtom,
-    editorPromptTimesAtom,
+    editorPromptTimesAtom, editorRightAtom, editorWidthAtom,
     editorYamlTimesAtom,
     showShareAtom
 } from "~app/state";
@@ -27,7 +27,13 @@ import store from "store2";
 import {Ace, EditSession, Range} from "ace-builds";
 import { addCompleter } from 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/mode-javascript';
-import shareIcon from '~/assets/icons/share.svg'
+import 'ace-builds/src-noconflict/snippets/javascript';
+
+import uploadIcon from '~/assets/icons/share.svg'
+import exportIcon from '~/assets/icons/export.svg'
+import importIcon from '~/assets/icons/import.svg'
+
+import {NumberSize, Resizable} from 're-resizable';
 
 import Browser from "webextension-polyfill";
 import {BiCloudUpload, BiExport, BiImport, BiShareAlt, BiUpload} from "react-icons/bi";
@@ -37,6 +43,9 @@ import Tooltip from "~app/components/Tooltip";
 import discordIcon from "~assets/icons/discord.svg";
 import {requestHostPermission} from "~app/utils/permissions";
 import {ChatError, ErrorCode} from "~utils/errors";
+import layoutBottomIcon from "~assets/icons/editor-layout-bottom.svg";
+import layoutRightIcon from "~assets/icons/editor-layout-right.svg";
+import {Direction} from "re-resizable/lib/resizer";
 
 interface Props {
     setShowEditor: (show: boolean) => void;
@@ -48,12 +57,55 @@ function PromptForm(props: {setShowEditor: (show: boolean) => void;  }) {
     const [editorPrompt, setEditorPrompt] = useAtom(editorPromptAtom)
     const [editorPromptTimes, setEditorPromptTimes] = useAtom(editorPromptTimesAtom)
     const [editorYamlTimes, setEditorYamlTimes] = useAtom(editorYamlTimesAtom)
+    const [resizeKey, setResizeKey] = useState(0)
     const { t } = useTranslation()
     const localPromptsQuery = useSWR('local-prompts', () => loadLocalPrompts(), { suspense: true})
     const confirmTips = t('Are you sure you want to import the Agent?')
     const successTips = t('Imported Agent successfully')
     const [shareViewShow, setShowShareView] = useAtom(showShareAtom)
     const [editorFocus, setEditorFocus] = useAtom(editorFocusAtom)
+
+    const [editorWidth, setEditorWidth] = useAtom(editorWidthAtom)
+    const [editorHeight, setEditorHeight] = useAtom(editorHeightAtom)
+    const [editorRight, setEditorRightAtom] = useAtom(editorRightAtom)
+
+    const [yamlResizableRef, setYamlResizableRef] = useState<Resizable | null>(null);
+    const [funcResizableRef, setFuncResizableRef] = useState<Resizable | null>(null);
+    const [promptIDERef, setPromptIDERef] = useState<HTMLDivElement | null>(null);
+
+    interface Enable {
+        top?: boolean;
+        right?: boolean;
+        bottom?: boolean;
+        left?: boolean;
+        topRight?: boolean;
+        bottomRight?: boolean;
+        bottomLeft?: boolean;
+        topLeft?: boolean;
+    }
+
+    let resizeEnableYaml = {
+        top: false,
+        right: true,
+        bottom: true,
+        left: false,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false
+    } as Enable
+
+    const resizeEnableFunc = {
+        top: false,
+        right: false,
+        bottom: false,
+        left: false,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false
+    } as Enable
+
     type SelectionValue = {
         anchor: {
             row: number;
@@ -77,6 +129,7 @@ function PromptForm(props: {setShowEditor: (show: boolean) => void;  }) {
     }
 
     function getEditYaml() {
+        update(editorRight)
         const prompts = getStore("prompts", null);
         if (prompts !== null && prompts[getStore("real_yaml", "Default_Flow_Dag_Yaml")]){
             return prompts[getStore("real_yaml", "Default_Flow_Dag_Yaml")]
@@ -317,57 +370,187 @@ function PromptForm(props: {setShowEditor: (show: boolean) => void;  }) {
         setShowShareView(true)
     }
 
+    const update = useCallback((editorRight: boolean) => {
+        console.log("editor right: " + editorRight)
+
+        if (promptIDERef && promptIDERef.offsetWidth>0) {
+            const width = promptIDERef.offsetWidth
+            const height = promptIDERef.offsetHeight
+
+            if (editorRight){
+                let yamlWidth = editorWidth
+                if (yamlWidth == 0){
+                    yamlWidth = width/2
+                }else if (width == yamlWidth){
+                    yamlWidth = width - 20
+                }
+
+                if (yamlResizableRef) {
+
+                    yamlResizableRef.updateSize({width: yamlWidth, height: height});
+                }
+
+                if (funcResizableRef) {
+                    let funcWidth = width - yamlWidth
+                    if (funcWidth == 0){
+                        funcWidth = width/2
+                    }
+                    console.log("update size func: " + funcWidth + ", " + height)
+                    funcResizableRef.updateSize({width: funcWidth, height: height});
+                }
+            }else{
+                let yamlHeight = editorHeight
+                if (yamlHeight == 0){
+                    yamlHeight = height/2
+                }else if (yamlHeight == height){
+                    yamlHeight = height - 20
+                }
+
+                if (yamlResizableRef) {
+                    yamlResizableRef.updateSize({width: width, height: yamlHeight});
+                }
+
+                if (funcResizableRef) {
+                    let funcHeight = height - yamlHeight
+                    if (funcHeight == 0){
+                        funcHeight = width/2
+                    }
+                    console.log("update size func: " + width + ", " + funcHeight)
+                    funcResizableRef.updateSize({width: width, height: funcHeight});
+                }
+            }
+
+        }
+
+    },[resizeEnableFunc, resizeEnableYaml])
+
+    function onResizeYaml(event: MouseEvent | TouchEvent, direction: Direction, elementRef: HTMLElement, delta: NumberSize){
+        console.log("onResizeYaml")
+        if (yamlResizableRef){
+            const yamlWidth = elementRef.offsetWidth
+            const yamlHeight = elementRef.offsetHeight
+            setEditorWidth(yamlWidth)
+            setEditorHeight(yamlHeight)
+        }
+        update(editorRight)
+    }
+
+    function onResizeFunc(event: MouseEvent | TouchEvent, direction: Direction, elementRef: HTMLElement, delta: NumberSize){
+        console.log("onResizeFunc")
+    }
+
+    const layoutChange = useCallback(() => {
+        const newState = !editorRight
+        if (promptIDERef) {
+            if (!newState) {
+                setEditorWidth(promptIDERef.offsetWidth)
+                setEditorHeight(promptIDERef.offsetHeight / 2)
+            } else {
+                setEditorWidth(promptIDERef.offsetWidth / 2)
+                setEditorHeight(promptIDERef.offsetHeight)
+            }
+            setEditorRightAtom(newState)
+            update(newState)
+        }
+        setResizeKey(resizeKey+1)
+    }, [editorRight, resizeEnableFunc, resizeKey, setResizeKey])
+
     return (
-        <div className="overflow-auto h-full flex flex-col promptide">
-            <div className="flex items-left mx-10 margin-5">
+        <div className="h-full flex flex-col promptide" ref={c => { setPromptIDERef(c); }}>
+            <div className="flex items-left mx-3 margin-5">
                 <div className="flex flex-row gap-3">
-                    <Button size="small" text={t('Export ALL')} icon={<BiExport />} onClick={exportAgentAll} />
-                    <Button size="small" text={t('Export Current')} icon={<BiExport />} onClick={exportPromptFlow} />
-                    <Button size="small" text={t('Import')} icon={<BiImport />} onClick={importYaml} />
-                    <Tooltip content={t('Upload')}>
-                        <Button size="small" text={t('Upload')} icon={<BiCloudUpload />} onClick={shareGpts} />
+                    <button type="button" className={cx('rounded-xl', 'text-sm px-4 py-1', 'text-primary-text bg-secondary')} onClick={layoutChange}>
+                        <div className="flex flex-row items-center gap-1 min-w-max">
+                            <img src={editorRight?layoutBottomIcon:layoutRightIcon}/>
+                        </div>
+                    </button>
+                    {/*<Button size="small" text={t('Export ALL')} icon={<BiExport />} onClick={exportAgentAll} />*/}
+                    <Tooltip content={t('Export: Exporting the Current Agent')}>
+                        <button type="button" className={cx('rounded-xl', 'text-sm px-4 py-1', 'text-primary-text bg-secondary')} onClick={exportPromptFlow}>
+                            <div className="flex flex-row items-center gap-1 min-w-max">
+                                <img src={exportIcon}/>
+                            </div>
+                        </button>
+                    </Tooltip>
+                    <Tooltip content={t('Import: Import Agents from file')}>
+                        <button type="button" className={cx('rounded-xl', 'text-sm px-4 py-1', 'text-primary-text bg-secondary')} onClick={importYaml}>
+                            <div className="flex flex-row items-center gap-1 min-w-max">
+                                <img src={importIcon}/>
+                            </div>
+                        </button>
+                    </Tooltip>
+                    <Tooltip content={t('Upload: Generate a link that details the Agent')}>
+                        <button type="button" className={cx('rounded-xl', 'text-sm px-4 py-1', 'text-primary-text bg-secondary')} onClick={shareGpts}>
+                            <div className="flex flex-row items-center gap-1 min-w-max">
+                                <img src={uploadIcon}/>
+                            </div>
+                        </button>
                     </Tooltip>
                 </div>
             </div>
-            <div className="overflow-auto h-full flex flex-cow" key={editorYamlTimes}>
-                <AceEditor
-                    mode="yaml"
-                    theme="github"
-                    name="prompt"
-                    width="50%"
-                    height="100%"
-                    onChange={onChangeYaml}
-                    onSelectionChange={onSelectionChange}
-                    onFocus={onFocusYaml}
-                    onBlur={onFocusYaml}
-                    defaultValue={getEditYaml()}
-                    editorProps={{ $blockScrolling: true }}
-                    setOptions={{
-                        useWorker: false,
-                        enableBasicAutocompletion: true,
-                        enableLiveAutocompletion: true,
-                        enableSnippets: true
-                    }}
-                />
-                <AceEditor
-                    key={editorPromptTimes}
-                    mode="javascript"
-                    theme="github"
-                    name="prompt-func"
-                    width="50%"
-                    height="100%"
-                    onChange={onChangePrompt}
-                    onFocus={onFocusPrompt}
-                    onBlur={onFocusPrompt}
-                    defaultValue={getEditPrompt()}
-                    editorProps={{ $blockScrolling: true }}
-                    setOptions={{
-                        useWorker: false,
-                        enableBasicAutocompletion: true,
-                        enableLiveAutocompletion: true,
-                        enableSnippets: true
-                    }}
-                />
+            <div className={"h-full flex " + (editorRight?"flex-cow":"flex-col")} key={editorYamlTimes}>
+
+                <Resizable
+                    className="editor-yaml-margin"
+                    ref={c => { setYamlResizableRef(c); }}
+                    enable={resizeEnableYaml}
+                    defaultSize={promptIDERef?{
+                        width: editorRight?promptIDERef.offsetWidth/2:promptIDERef.offsetWidth,
+                        height: editorRight?promptIDERef.offsetHeight: promptIDERef.offsetHeight/2,
+                    }:undefined}
+                    size={yamlResizableRef ? { width: yamlResizableRef.state?.width, height: yamlResizableRef.state?.height } : undefined}
+                    onResizeStop={onResizeYaml}>
+                    <AceEditor
+                        mode="yaml"
+                        theme="github"
+                        name="prompt"
+                        width="100%"
+                        height="100%"
+                        onChange={onChangeYaml}
+                        onSelectionChange={onSelectionChange}
+                        onFocus={onFocusYaml}
+                        onBlur={onFocusYaml}
+                        defaultValue={getEditYaml()}
+                        editorProps={{ $blockScrolling: true }}
+                        setOptions={{
+                            useWorker: false,
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: true
+                        }}
+                    />
+                </Resizable>
+                <Resizable
+                    key={resizeKey}
+                    className="editor-func-border"
+                    defaultSize={promptIDERef?{
+                        width: editorRight?promptIDERef.offsetWidth/2:promptIDERef.offsetWidth,
+                        height: editorRight?promptIDERef.offsetHeight: promptIDERef.offsetHeight/2,
+                    }:undefined}
+                    ref={c => { setFuncResizableRef(c); }}
+                    enable={resizeEnableFunc}
+                    size={funcResizableRef ? { width: funcResizableRef.state?.width, height: funcResizableRef.state?.height } : undefined}
+                    onResizeStop={onResizeFunc}>
+                    <AceEditor
+                        key={editorPromptTimes}
+                        mode="javascript"
+                        theme="github"
+                        name="prompt-func"
+                        width="100%"
+                        height="100%"
+                        onChange={onChangePrompt}
+                        onFocus={onFocusPrompt}
+                        onBlur={onFocusPrompt}
+                        defaultValue={getEditPrompt()}
+                        editorProps={{ $blockScrolling: true }}
+                        setOptions={{
+                            useWorker: false,
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: true
+                        }}
+                    />
+                </Resizable>
             </div>
         </div>
     )
